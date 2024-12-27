315ed84dea48e0b7946087d1d4455a5618bf8be0
manabeai <100462113+manabeai@users.noreply.github.com>
2024-12-27 02:49:02 +0000

    環境変数を正しくした

--- sample/main.cpp
+++ sample/main.cpp
@@ -1,5 +1,11 @@
-#include <bits/stdc++.h>
 #include <atcoder/all>
+#include <bits/stdc++.h>
+#ifdef DEBUG
+#include "cpp-dump/dump.hpp"
+#define dump(...) cpp_dump(__VA_ARGS__)
+#else
+#define dump(...)
+#endif
 using namespace std;
 using namespace atcoder;
 typedef long long ll;
